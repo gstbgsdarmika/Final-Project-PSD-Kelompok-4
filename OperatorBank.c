e0be206f0f1dad3ac1d819935f6fbc842b06dfa4
Aruvins <70146223+Alvynst@users.noreply.github.com>
2021-12-15 18:59:54 +0800

    Operator Bank Sederhana

---
+++ OperatorBank.c
@@ -0,0 +1,36 @@
+int main()
+{
+    do
+    {
+    
+    
+    switch(menu)
+     {
+        case 1:
+        
+        case 2:
+        
+        case 3:
+        
+        case 4:
+        
+        case 5 :
+        
+        case 6:
+        
+        case 7:
+        
+        case 8:
+       
+        break;
+        default:
+        printf("Mohon maaf menu yg anda pilih salah");
+        break;
+    }
+    printf("\nApakah anda ingin mengulang? Input 1 jika ya : ");
+    scanf("%d", &ulang);
+
+    } while (ulang == 1);
+
+    return;
+}
