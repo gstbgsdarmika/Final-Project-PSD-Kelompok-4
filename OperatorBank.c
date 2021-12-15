f3c7db9f39659464f5c06492410681e984b06ea7
Kartika Noviyanti <84194532+kartika28@users.noreply.github.com>
2021-12-16 01:40:11 +0800

    Update OperatorBank.c
    Menambahkan function mengecek identitas nasabah (cekident)
    Menambahkan function menghapus nasabah (hapusdata)
    Menambahkan code pada case 2 di function main (hapus nasabah)
    Merapikan code ketika ingin mengulang program

--- OperatorBank.c
+++ OperatorBank.c
@@ -3,6 +3,74 @@
 #include <string.h>
 #include <conio.h>
 
+//function untuk mengecek identitas nasabah
+void cekident(asal) {
+    int password;
+    search = head;
+    if(head == NULL){
+      printf("\n\t\t\t    Tidak ada data nasabah\n");
+      printf("\n\t\t\t Tekan apa saja untuk kembali\n");
+      getch();
+      main();
+    }
+
+   while(search!=NULL){
+      if(search->nomor == asal){
+         printf("\n\t\t Nama Nasabah       : %s\n", search->nama);
+         printf("\n\t\t Masukkan password  : ");
+         scanf("%d", &password);
+         if (search->pass != password){
+            printf("\n\t   Mohon maaf password salah");
+            printf("\n\t Tekan apa saja untuk kembali");
+            getch();
+            main();
+         } else if ((search->pass == password)) {
+                printf("\n\t Login berhasil, \n");
+                return;
+           }
+      } if(search->next != NULL) {
+          search = search->next;
+        } else {
+            printf("\n\t\t\t Identitas nasabah tidak ditemukan\n");
+            printf("\n\t\t\t   Tekan apa saja untuk kembali\n");
+            getch();
+            main();
+          }
+     }
+}
+
+//function untuk menghapus data nasabah pada menu kedua
+void hapusdata(int delete){
+   if (head == NULL){
+      printf("\n\t\t\t\t Tidak ada data nasabah\n");
+      return;
+   }
+
+   hapus = head;
+
+   while(hapus!=NULL) {
+      if(hapus->nomor == delete) {
+         if (hapus == head) {
+             head = head->next;
+             free(hapus);
+             hapus = head;
+             printf("\n\t\t\t Data nasabah terhapus\n");
+             jumlahnode--;
+             return;
+          } else {
+                head->prev->next = hapus->next;
+                free(hapus);
+                printf("\n\t\t\t Data nasabah terhapus\n");
+                hapus = head->prev->next;
+                jumlahnode--;
+              }
+       } else {
+            head->prev = hapus;
+            hapus = hapus->next;
+           }
+    }
+}
+
 int main()
 {
     int menu;
@@ -15,7 +83,14 @@ int main()
         case 1:
         
         case 2:
-        
+        system("cls");
+            printf("\n\n=========================::::: Hapus Data Nasabah :::::=========================\n");
+            printf("\n\t Masukkan Identitas Nasabah yang ingin Dihapus\n");
+            printf("\n\t\t Nomor identitas    : ");
+            scanf("%d", &del);
+            cekident(del);
+            hapusdata(del);
+            break;
         case 3:
         
         case 4:
@@ -30,13 +105,16 @@ int main()
        
         break;
         default:
-        printf("Mohon maaf menu yg anda pilih salah");
+        printf("\n\t\t\t Mohon maaf menu yg anda pilih salah\n");
         break;
     }
-    printf("\nApakah anda ingin mengulang? Input 1 jika ya : ");
+    printf("\n=================================================================================\n");
+    printf("\nApakah anda ingin mengulang program ? ");
+    printf("\n\t\t 1. Ya\n");
+    printf("\n\t\t 2. Tidak\n");
+    printf("\n\t Masukkan Pilihan Anda  : ");
     scanf("%d", &ulang);
 
     } while (ulang == 1);
-
     return;
 }
